ddc19363b68292467af67edd28cc05cf8b78eb6f
yooksi <yooksi@zoho.com>
2019-05-15 03:14:20 +0200

    Create simple ItemBase class

---
+++ src/main/java/io/yooksi/forgelib/ItemBase.java
@@ -0,0 +1,35 @@
+package io.yooksi.forgelib;
+
+import io.yooksi.commons.define.MethodsNotNull;
+import io.yooksi.commons.logger.CommonLogger;
+import io.yooksi.commons.validator.BeanValidator;
+import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
+
+@MethodsNotNull
+public class ItemBase extends Item {
+
+    public ItemBase(@ForgeRegIdentifier String id, ForgeMod mod) {
+        
+        setRegistryName(id);
+        setTranslationKey(id);
+        setCreativeTab(mod.getCreativeTab());
+        CommonLogger.get().debug("Created new item %s for mod %s", id, mod);
+    }
+
+    /**
+     * Helper method to construct and validate new simple custom items.
+     *
+     * @param itemClass main class of the item to construct
+     * @param name simple item name <i>(excluding namespace)</i>
+     * @param mod instance of the mod constructing the item
+     * @return newly constructed and validated item instance
+     */
+    public static <T extends ItemBase> T construct(Class<T> itemClass, String name, ForgeMod mod) {
+
+        ResourceLocation id = new ResourceLocation(mod.getModId(), name);
+        CommonLogger.get().debug("Constructing new item %s(%s) for mod %s", name, id, mod);
+
+        return BeanValidator.constructChild(ItemBase.class, itemClass, id.toString(), mod);
+    }
+}
